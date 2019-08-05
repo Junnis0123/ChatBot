af1aceb8325df9f0227d1ce1075d85aa56cdd5a7
Junnis0123 <33163151+Junnis0123@users.noreply.github.com>
2019-08-05 15:21:02 +0900

    Add files via upload

---
+++ MailBot.py
@@ -0,0 +1,67 @@
+﻿import telegram
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler  # import modules
+import ParsingMail
+from time import sleep
+import threading, time
+
+token = ''
+chat_id = ''
+
+# start
+def start(bot, update) :
+    global chat_id
+    chat_id = update.message.chat.id
+    update.message.reply_text('start to read mail.' )
+        
+    t = threading.Thread(target=Check_mail)
+    t.daemon = True
+    t.start()
+
+
+# change from people
+def chnage_from_people(bot, update) :
+    name = update.message.text.split()
+    if len(name) == 1:
+        update.message.reply_text('Command is /change newName')
+        return
+    ParsingMail.change_from_name(name[1])
+    update.message.reply_text('{}{}{}'.format('from person is  changed by ', name[1], ' ))
+
+
+# print from people
+def print_from_people(bot, update) :
+    update.message.reply_text('{}{}{}'.format('To person is ', ParsingMail.get_name(), ' ))
+
+
+#check Mail
+def Check_mail():
+    while True:
+        count, data = ParsingMail.ConnectMailSvr()
+        if count == 0:
+            sleep(20)
+            continue
+        else:
+            bot = telegram.Bot(token = token)
+            bot.sendMessage(chat_id = chat_id, text='unseen mail is {}, new is {}'.format(count, len(data)))
+
+            for mail in data:
+                bot.sendMessage(chat_id = chat_id, text='subject : {}\ncontext : {}'.format(mail[0],mail[1]))
+            sleep(60)
+            
+
+if __name__ == "__main__":
+
+    updater = Updater(token)
+
+    print_handler = CommandHandler('print', print_from_people)
+    updater.dispatcher.add_handler(print_handler)
+
+    change_handler = CommandHandler('change', chnage_from_people)
+    updater.dispatcher.add_handler(change_handler)
+
+    start_handler = CommandHandler('start', start)
+    updater.dispatcher.add_handler(start_handler)
+
+    updater.start_polling(timeout=3, clean=True)
+    updater.idle()
+
---
+++ ParsingMail.py
@@ -0,0 +1,104 @@
+﻿import imaplib
+import email
+from time import sleep
+from email.header import decode_header
+import base64
+
+#USER INFORMATION
+UserId = ''
+Password = ''
+
+#MAIL SERVER SETTING
+Server = ''
+Port = 993
+
+#FROM INFO
+fromName = ''
+fromMail = '{}{}{}'.format('(UNSEEN from ', fromName, ')')
+
+#MAILINFO
+latest = ''
+
+def change_from_name(name):
+    global fromName
+    fromName = name
+    global fromMail
+    fromMail = '{}{}{}'.format('(UNSEEN from ', fromName, ')')
+
+
+def get_name():
+    return fromName
+
+def ConnectMailSvr():
+    print('Connect To ' + UserId + "...")
+    mail = imaplib.IMAP4_SSL(Server, Port)
+    mail.login(UserId, Password)
+    mail.select("inbox")
+    print('Connect!')
+    _, unseen_data = mail.search(None, fromMail)
+    
+    email_ids  = unseen_data[0].split()
+
+    if len(email_ids) == 0:
+        mail.close() 
+        return 0, 0
+
+    latest_id = email_ids[-1:][0]
+    global latest
+
+    if latest_id == latest:
+        mail.close() 
+        return 0, 0
+    
+    if latest == '': #first
+        latest = email_ids[0]
+        eachIdsList = email_ids[  email_ids.index(latest) : email_ids.index(latest_id) + 1 ]
+    else:
+        eachIdsList = email_ids[  email_ids.index(latest) + 1 : email_ids.index(latest_id) + 1 ]
+    latest = latest_id
+
+    result =  str(len(email_ids))
+    print('unseen mail is {}, new unseen mail is {}.'.format(result, len(eachIdsList)))    
+    
+
+    mails = []
+    for e_id in eachIdsList:
+        _, data = mail.fetch(e_id, '(RFC822)')
+        mails.append(GetContents(data))
+
+   
+    mail.close() 
+    print('DisConnect!')
+    return result, mails
+
+def GetContents(data):
+    raw_email = data[0][1]
+
+    raw_email_string = raw_email.decode('utf-8')        
+    email_message = email.message_from_string(raw_email_string)
+ 
+    print (email.utils.parseaddr(email_message['From'])[1]) 
+    subject = email_message['Subject']
+
+    if len(subject) != 0:
+        subject, encoding = decode_header(subject)[0]
+        subject = subject.decode(encoding)
+    else:
+        subject = '()'
+    
+    print(subject)
+
+     # Contents
+    while email_message.is_multipart():
+        email_message = email_message.get_payload(0)
+
+    content = email_message.get_payload()
+    content = base64.b64decode(content).decode()
+    print(content)
+    return subject, content
+
+
+'''
+if __name__ == "__main__":
+    GetMailLoop()
+'''
